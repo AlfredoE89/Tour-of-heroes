d7c672ab92d44e72a47727ae00c16669ef1c5ce8
AlfredoE89 <w0lf_0f_wall_street@icloud.com>
2019-11-12 20:08:16 -0600

    Generated the users component.
    git add .

--- ng-auth/src/app/services/user.service.ts
+++ ng-auth/src/app/services/user.service.ts
@@ -1,12 +1,17 @@
-
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    // 5. Inject HttpClient into the constructor
+    private http:HttpClient
+  ) { }
 
   test(): string{
     return 'success!';
