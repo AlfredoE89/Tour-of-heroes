768d9358a086382107a5e3e87db70f1887bdf1a5
AlfredoE89 <w0lf_0f_wall_street@icloud.com>
2019-11-13 17:33:33 -0600

    articles section

--- ng-users/src/app/app-routing.module.ts
+++ ng-users/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
 
+// 1. Routing Libraries
+import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent }   from './users/users.component';
+import { UserViewComponent }   from './user-view/user-view.component';
+import { UserCreateComponent }   from './user-create/user-create.component';
+import { UserEditComponent }   from './user-edit/user-edit.component';
+import { ArticlesComponent }   from './articles/articles.component';
 
-const routes: Routes = [];
+// 3. Declare your routes
+const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent },
+  { path: 'users/view/:id', component: UserViewComponent },
+  { path: 'users/create', component: UserCreateComponent },
+  { path: 'users/edit/:id', component: UserEditComponent },
+  { path: 'articles', component: ArticlesComponent }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- ng-users/src/app/app.module.ts
+++ ng-users/src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { HttpClientModule } from '@angular/common/http';
+import { UserViewComponent } from './user-view/user-view.component';
+import { UserCreateComponent } from './user-create/user-create.component';
+import { FormsModule }   from '@angular/forms';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserViewComponent,
+    UserCreateComponent,
+    UserEditComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
